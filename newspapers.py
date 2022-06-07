ce1ccaceb9f54b309cef547e731d22c14dc4ccc1
kamil Kuczyński <kamil.kuczynski4+github@gmail.com>
2022-06-07 10:34:51 +0200

    add articles from foxnews.com

--- newspapers.py
+++ newspapers.py
@@ -5,19 +5,28 @@
 #TODO 5:
 
 import pprint
-
 import bs4
 import requests
 
 raw_data = requests.get("https://www.aljazeera.com/")
+fox_data = requests.get("https://www.foxnews.com/")
+# print(fox_data)
 
 soup = bs4.BeautifulSoup(raw_data.text, 'lxml')
+fox_soup = bs4.BeautifulSoup(fox_data.text, 'lxml')
+# print(fox_soup.text)
+
+
 titles = soup.select('.article-trending.u-clickable-card')
-# print(titles)
+fox_titles = fox_soup.select('.title > a')
+# pprint.pprint(fox_titles)
 
 list_of_titles = []
 
 for title in titles:
+    list_of_titles.append(title.text)
+
+for title in fox_titles[1:11]:
     list_of_titles.append(title.text)
 
 pprint.pprint(list_of_titles)
