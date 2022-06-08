a88664a99264cf9f3325b1adeccb54a1b4beb1fc
kamil Kuczyński <kamil.kuczynski4+github@gmail.com>
2022-06-08 09:52:17 +0200

    make list of titles from German dw.de

--- newspapers.py
+++ newspapers.py
@@ -10,23 +10,38 @@ import requests
 
 raw_data = requests.get("https://www.aljazeera.com/")
 fox_data = requests.get("https://www.foxnews.com/")
+dw_germany_data = requests.get("https://www.dw.com/en/germany/s-1432")
 # print(fox_data)
 
 soup = bs4.BeautifulSoup(raw_data.text, 'lxml')
 fox_soup = bs4.BeautifulSoup(fox_data.text, 'lxml')
+dw_german_soup = bs4.BeautifulSoup(dw_germany_data.text, 'lxml')
 # print(fox_soup.text)
 
 
 titles = soup.select('.article-trending.u-clickable-card')
 fox_titles = fox_soup.select('.title > a')
+dw_titles = dw_german_soup.select('h2')
 # pprint.pprint(fox_titles)
 
 list_of_titles = []
 
-for title in titles:
+print("Al jazeera")
+for title in titles[0:5]:
     list_of_titles.append(title.text)
+pprint.pprint(list_of_titles[0:7])
 
+print()
+print("Fox News")
 for title in fox_titles[1:11]:
     list_of_titles.append(title.text)
+pprint.pprint(list_of_titles[7:12])
 
-pprint.pprint(list_of_titles)
+print()
+german_titles = []
+print("Deutsche Welle - Germany")
+for title in dw_titles[1:7]:
+    german_titles.append(title.text.replace("\n" ,""))
+pprint.pprint(german_titles)
+
+# pprint.pprint(list_of_titles)
