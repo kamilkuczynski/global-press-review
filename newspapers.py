acd2f1fadd95de3cca743cd9df90ce284a8bed74
kamil Kuczyński <kamil.kuczynski4+github@gmail.com>
2022-06-12 11:11:59 +0200

    add news from Chinese newspaper

--- newspapers.py
+++ newspapers.py
@@ -7,19 +7,20 @@
 import pprint
 import bs4
 import requests
+import lxml
 
 raw_data = requests.get("https://www.aljazeera.com/")
 fox_data = requests.get("https://www.foxnews.com/")
 dw_germany_data = requests.get("https://www.dw.com/en/germany/s-1432")
 russian_today_data = requests.get("https://www.rt.com/")
-scmp_data = requests.get('https://www.scmp.com/rss/318198/feed')
-# print(fox_data)
+scmp_data = requests.get('https://www.scmp.com/news/china?module=mega_menu_news_int&pgtype=homepage')
+print(scmp_data)
 
 soup = bs4.BeautifulSoup(raw_data.text, 'lxml')
 fox_soup = bs4.BeautifulSoup(fox_data.text, 'lxml')
 dw_german_soup = bs4.BeautifulSoup(dw_germany_data.text, 'lxml')
 russian_soup = bs4.BeautifulSoup(russian_today_data.text, 'lxml')
-scmp_soup = bs4.BeautifulSoup(scmp_data.text, 'lxml')
+scmp_soup = bs4.BeautifulSoup(scmp_data.text, 'html.parser')
 # pprint.pprint(scmp_soup)
 
 
@@ -27,11 +28,11 @@ titles = soup.select('.article-trending.u-clickable-card')
 fox_titles = fox_soup.select('.title > a')
 dw_titles = dw_german_soup.select('.news')
 russian_titles = russian_soup.select('.card')
-scmp_titles = scmp_soup.select('item')
+scmp_titles = scmp_soup.select('.article__title')
 # pprint.pprint(scmp_titles)
 
 list_of_titles = []
-'''
+
 print("Al jazeera")
 for title in titles[0:5]:
     list_of_titles.append(title.text)
@@ -60,16 +61,16 @@ for title in russian_titles:
     # print(title.text.)
 
 pprint.pprint(russian_list_titles)
-'''
-#
+
 print()
 # print("Politics - South China Morning Post")
-for title in scmp_soup.select("item")[1:5]:
-    pprint.pprint(title.select('title')[0].text)
-    pprint.pprint(title.select('link')[0].text)
-    pprint.pprint(title.select('description')[0].text)
+for title in scmp_titles[1:6]:
+    pprint.pprint(title.text.splitlines()[0])
+    pprint.pprint("https://www.scmp.com"+ title.select("a")[0]['href'])
     print()
     # list_of_titles.append(title.text.replace("\n",""))
 # pprint.pprint(list_of_titles[7:12])
-# pprint.pprint(scmp_soup.select('title'))
-# pprint.pprint(scmp_soup.select('link'))
+
+
+
+
