28a580ca6f18208473fc3e45a525970855263d82
kamil Kuczyński <kamil.kuczynski4+github@gmail.com>
2022-06-13 08:38:45 +0200

    add function to save news to file in txt format

--- newspapers.py
+++ newspapers.py
@@ -1,13 +1,14 @@
-#TODO 1: dodać linki
-#TODO 2: dodać foxnews, dw.de, russian Today, France24, any Chinese
-#TODO 3: dodać zapis do txt
-#TODO 4: zrobić gui w tkinker
-#TODO 5:
+# TODO 1: dodać linki
+# TODO 2: dodać foxnews, dw.de, russian Today, France24, any Chinese
+# TODO 3: dodać zapis do txt
+# TODO 4: zrobić gui w tkinker
+# TODO 5:
 
 import pprint
 import bs4
 import requests
 import lxml
+from datetime import datetime
 
 raw_data = requests.get("https://www.aljazeera.com/")
 fox_data = requests.get("https://www.foxnews.com/")
@@ -41,36 +42,49 @@ pprint.pprint(list_of_titles[0:7])
 print()
 print("Fox News")
 for title in fox_titles[1:11]:
-    list_of_titles.append(title.text.replace("\n",""))
-pprint.pprint(list_of_titles[7:12])
+    list_of_titles.append(title.text.replace("\n", ""))
+    print(title.text)
+# pprint.pprint(list_of_titles[7:12])
 
 
 print()
 german_titles = []
-print("Deutsche Welle - Germany")
-for title in dw_titles[1:6]:
-    print(title.select('h2')[0].text, "\n", "https://www.dw.com"+title.select('a')[0]['href'])
 
-    # german_titles.append(title.text.replace("\n" ,""))
-pprint.pprint(german_titles)
+print("Deutsche Welle - Germany")
+
+for title in dw_titles[1:6]:
+    print(title.select('h2')[0].text, "\n",
+          "https://www.dw.com" + title.select('a')[0]['href'])
+
+    german_titles.append(title.text.replace("\n", ""))
+    list_of_titles.append(title.text.replace("\n", ""))
 
 print("Russian Today - Russia")
 russian_list_titles = []
-for title in russian_titles:
-    russian_list_titles.append(title.text)
-    # print(title.text.)
+for title in russian_titles[:5]:
+    # print(title.get_text().splitlines())
+    russian_list_titles.append(title.text.strip())
+    russian_list_titles.append(title.select('a'))
+    list_of_titles.append(title.text.strip())
 
 pprint.pprint(russian_list_titles)
 
 print()
-# print("Politics - South China Morning Post")
+print("Politics - South China Morning Post")
+
 for title in scmp_titles[1:6]:
     pprint.pprint(title.text.splitlines()[0])
-    pprint.pprint("https://www.scmp.com"+ title.select("a")[0]['href'])
+    pprint.pprint("https://www.scmp.com" + title.select("a")[0]['href'])
     print()
-    # list_of_titles.append(title.text.replace("\n",""))
+    list_of_titles.append(title.text.splitlines()[0] +
+                          "https://www.scmp.com" + title.select("a")[0]['href'])
 # pprint.pprint(list_of_titles[7:12])
 
+# SAVE data to a file
 
 
+date = datetime.today().strftime("%Y_%m_%d")
 
+with open(date + '_daily_word_news_review.txt', "w") as file:
+    for items in list_of_titles:
+        file.writelines([items + '\n' + '\n'])
