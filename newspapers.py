cbf11443f82b4174530b7b2236b1211ca4ad9320
kamil Kuczyński <kamil.kuczynski4+github@gmail.com>
2023-02-15 07:34:30 +0100

    Add functions and fix titles for Chinese newspaper

--- newspapers.py
+++ newspapers.py
@@ -3,89 +3,80 @@
 # TODO 3: dodać zapis do txt
 # TODO 4: zrobić gui w tkinker
 
-
-import bs4
-import requests
 from datetime import datetime
+import bs4
+import config
+import requests
+import smtplib
 
-raw_data = requests.get("https://www.aljazeera.com/")
-fox_data = requests.get("https://www.foxnews.com/")
-dw_germany_data = requests.get("https://www.dw.com/")
-scmp_data = requests.get('https://www.scmp.com/news/china?module=mega_menu_news_int&pgtype=homepage')
-print(scmp_data)
+def fetch_soup(url):
+    response = requests.get(url)
+    return bs4.BeautifulSoup(response.text, 'html.parser')
 
-soup = bs4.BeautifulSoup(raw_data.text, 'lxml')
-fox_soup = bs4.BeautifulSoup(fox_data.text, 'lxml')
-dw_german_soup = bs4.BeautifulSoup(dw_germany_data.text, 'lxml')
-scmp_soup = bs4.BeautifulSoup(scmp_data.text, 'html.parser')
+def fetch_aljazeera_titles():
+    soup = fetch_soup('https://www.aljazeera.com/')
+    titles = soup.select('.article-trending__content')
+    return [(title.text, 'https://www.aljazeera.com' + title.a['href']) for title in titles[0:5]]
 
+def fetch_foxnews_titles():
+    soup = fetch_soup('https://www.foxnews.com/')
+    fox_titles = soup.select('.title > a')
+    return [(title.text.replace("\n", ""), title['href']) for title in fox_titles[1:5]]
 
-titles = soup.select('.article-trending__content')
-fox_titles = fox_soup.select('.title > a')
-dw_titles = dw_german_soup.select('.news')
-scmp_titles = scmp_soup.select('.article__title')
-# pprint.pprint(scmp_titles)
+def fetch_scmp_titles():
+    soup = fetch_soup('https://www.scmp.com/news/china')
+    titles = soup.find_all('h2', class_='article__title')
+    seen_titles = set()
+    result = []
+    for title in titles[1:8]:
+        link = title.find('a')['href']
+        title_text = title.get_text().strip()
+        if title_text not in seen_titles:
+            result.append((title_text, f'https://www.scmp.com{link}'))
+            seen_titles.add(title_text)
+    return result
 
-list_of_titles = []
+def save_to_file(data):
+    date = datetime.today().strftime("%Y_%m_%d")
+    filename = f'{date}_daily_word_news_review.txt'
+    with open(filename, 'w') as file:
+        for title, url in data:
+            file.write(f'{title}\n{url}\n\n')
+    print(f'Data saved to file {filename}')
 
+def main():
+    aljazeera_titles = fetch_aljazeera_titles()
+    foxnews_titles = fetch_foxnews_titles()
+    scmp_titles = fetch_scmp_titles()
 
-print("===== Al jazeera =====")
-list_of_titles.append("===== Al jazeera =====")
+    print("===== Al jazeera =====")
+    for title, url in aljazeera_titles:
+        print(f'{title}\n{url}\n')
+    print("===== Fox News =====")
+    for title, url in foxnews_titles:
+        print(f'{title}\n{url}\n')
+    print("===== Politics - South China Morning Post =====")
+    for title, url in scmp_titles:
+        print(f'{title}\n{url}\n')
 
-for title in titles[0:5]:
-    print(title.text)
-    print("https://www.aljazeera.com/" + title.a['href'] + "\n")
-    list_of_titles.append(title.text + "\n"
-                          "https://www.aljazeera.com/" + title.a['href'] + "\n")
+    data = aljazeera_titles + foxnews_titles + scmp_titles
+    save_to_file(data)
 
+if __name__ == '__main__':
+    main()
 
-print()
-print("===== Fox News =====")
-list_of_titles.append("===== Fox News =====")
-
-for title in fox_titles[1:5]:
-    print(title.text)
-    print(title['href'] + "\n")
-    list_of_titles.append(title.text.replace("\n", "") + "\n" +
-                          title['href'] + "\n")
-
-
-print()
-german_titles = []
-print("=====Deutsche Welle - Germany=====")
-list_of_titles.append("=====Deutsche Welle - Germany=====")
-
-for title in dw_titles[1:6]:
-    print(title.select('h2')[0].text.strip())
-    print("https://www.dw.com" + title.select('a')[0]['href'] + "\n")
-    list_of_titles.append(title.select('h2')[0].text.strip() + "\n" +
-                          "https://www.dw.com" + title.select('a')[0]['href'] + "\n")
-
-
-print()
-print("=====Politics - South China Morning Post=====")
-list_of_titles.append("=====Politics - South China Morning Post=====")
-
-for title in scmp_titles[1:6]:
-    print(title.text.strip())
-    print(" https://www.scmp.com" + title.select("a")[0]['href']+ "\n")
-    #We can left an empty line by function print()
-
-    list_of_titles.append(title.text.splitlines()[0] +
-                          "\nhttps://www.scmp.com" + title.select("a")[0]['href'])
-# pprint.pprint(list_of_titles[7:12])
-
-
-# SAVE a data to a file
-
-date = datetime.today().strftime("%Y_%m_%d")
+# Send titles and links by email:
+email = "kamil.kuczynski4@gmail.com"
 
 try:
-    file =  open(date + '_daily_word_news_review.txt', 'w')
-    for items in list_of_titles:
-        file.writelines([items + '\n' + '\n'])
-except FileNotFoundError as error_message:
-    print("There was an error: ", error_message)
-finally:
-    file.close()
-    print("File was closed")
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=email, password=config.gmail_password)
+        connection.sendmail(from_addr=email,
+                            to_addrs="kamil.kuczynski4@gmail.com",
+                            msg=f"Subject:News from the World.\n\n "
+                                f"{list_of_titles}"
+                                f"\n Have a nice day".encode("utf-8"))
+
+except UnicodeEncodeError:
+    print("UnicodeEncodeError :(")
